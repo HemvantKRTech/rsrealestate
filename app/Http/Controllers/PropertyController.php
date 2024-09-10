15a2ce73863fb56402e804f197a2ec615977eb55
HemvantKRTec <hemvant@krtechnology.in>
2024-09-10 17:12:17 +0530

    update comes from mamoon sir

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -171,7 +171,7 @@ class PropertyController extends Controller
         // Pass properties and city data to the view
         return view('FrontendPages.property-mohali', [
             'properties' => $properties,
-            'city' => $city
+            'city' => $cityname
         ]);
     }
     public function sectorshow($sectorname)
