cc1100e8337b0ca76c9a7d51ce6a067a5701eb88
HemvantKRTech <hemvant@krtechnology.in>
2024-09-12 13:27:08 +0530

    search page done

---
+++ resources/views/FrontendPages/search-property.blade.php
@@ -0,0 +1,164 @@
+@extends('layouts.app')
+@section('content')
+@include('FrontendPages.partials.header')
+
+<div id="middle">
+      <div id="search-page" class="wrap thinFormat">
+         <div class="row1 dt columns12">
+            <div class="col-1 thinColumn">
+               <div class="column_Box mc thinColumnLink">
+                  <div class="tcl bullet divider uu dif">
+                     <div class="h h2">
+                        <b class="plusMinus"></b> 
+                        <p class="b dif rHead">Hot Property</p>
+                     </div>
+                     <div class="ic showHide_rp">
+                        <ul>
+                           <li>
+                              <b class="b2 fl"></b>
+                              <p class="ofh"><a href="http://www.chandigarhhouse.com/sell/3-bhk-flats-apartments-mohali_538330.htm" title="3 BHK Flats &amp; Apartments for Sale in Sector 70, Mohali">3 BHK Flats &amp; Apartments for Sale in Sector 70, Mohali</a></p>
+                           </li>
+                           <li>
+                              <b class="b2 fl"></b>
+                              <p class="ofh"><a href="http://www.chandigarhhouse.com/rent/4-bhk-flats-apartments-sector-68-mohali_538288.htm" title="4 BHK Flats &amp; Apartments for Rent in Sector 68, Mohali">4 BHK Flats &amp; Apartments for Rent in Sector 68, Mohali</a></p>
+                           </li>
+                           <li>
+                              <b class="b2 fl"></b>
+                              <p class="ofh"><a href="http://www.chandigarhhouse.com/rent/showrooms-sector-35-chandigarh_719156.htm" title="250 Sq.ft. Showrooms for Rent in Sector 35, Chandigarh">250 Sq.ft. Showrooms for Rent in Sector 35, Chandigarh</a></p>
+                           </li>
+                           <li>
+                              <b class="b2 fl"></b>
+                              <p class="ofh"><a href="http://www.chandigarhhouse.com/rent/showrooms-sector-70-mohali_702792.htm" title="990 Sq. Feet Showrooms for Rent in Sector 70, Mohali">990 Sq. Feet Showrooms for Rent in Sector 70, Mohali</a></p>
+                           </li>
+                           <li>
+                              <b class="b2 fl"></b>
+                              <p class="ofh"><a href="http://www.chandigarhhouse.com/sell/3-bhk-flats-apartments-sector-70-mohali_877954.htm" title="3 BHK Flats &amp; Apartments for Sale in Sector 70, Mohali">3 BHK Flats &amp; Apartments for Sale in Sector 70, Mohali</a></p>
+                           </li>
+                           <li>
+                              <b class="b2 fl"></b>
+                              <p class="ofh"><a href="http://www.chandigarhhouse.com/rent/3-bhk-flats-apartments-sector-38_581188.htm" title="3 BHK Flats &amp; Apartments for Rent in Sector 38">3 BHK Flats &amp; Apartments for Rent in Sector 38</a></p>
+                           </li>
+                           <li>
+                              <b class="b2 fl"></b>
+                              <p class="ofh"><a href="http://www.chandigarhhouse.com/sell/residential-plot-sector-79-mohali_719074.htm" title="Residential Plot for Sale in Sector 79, Mohali">Residential Plot for Sale in Sector 79, Mohali</a></p>
+                           </li>
+                           <li>
+                              <b class="b2 fl"></b>
+                              <p class="ofh"><a href="http://www.chandigarhhouse.com/rent/3-bhk-flats-apartments-sector-90-mohali_710755.htm" title="3 BHK Flats &amp; Apartments for Rent in Sector 90, Mohali">3 BHK Flats &amp; Apartments for Rent in Sector 90, Mohali</a></p>
+                           </li>
+                           <li>
+                              <b class="b2 fl"></b>
+                              <p class="ofh"><a href="http://www.chandigarhhouse.com/sell/3-bhk-flats-apartments-mohali_527061.htm" title="3 BHK Flats &amp; Apartments for Sale in Sector 91, Mohali">3 BHK Flats &amp; Apartments for Sale in Sector 91, Mohali</a></p>
+                           </li>
+                           <li>
+                              <b class="b2 fl"></b>
+                              <p class="ofh"><a href="http://www.chandigarhhouse.com/sell/residential-land-plot-sector-91-mohali_595251.htm" title="Residential Land / Plot for Sale in Sector 91, Mohali">Residential Land / Plot for Sale in Sector 91, Mohali</a></p>
+                           </li>
+                        </ul>
+                     </div>
+                  </div>
+               </div>
+               <div class="column_Box mc thinColumnLink dif">
+                  <div class="tcl bullet divider">
+                     <div class="h h2">
+                        <b class="plusMinus"></b> 
+                        <p class="b dif rHead">Property By City</p>
+                     </div>
+                     <div class="ic showHide_rp">
+                        <ul>
+                           <li>
+                              <b class="b2 fl"></b>
+                              <p class="ofh"><a href="http://www.chandigarhhouse.com/property-in-manimajra.htm" title="Property in Manimajra">Property in Manimajra</a></p>
+                           </li>
+                           <li>
+                              <b class="b2 fl"></b>
+                              <p class="ofh"><a href="http://www.chandigarhhouse.com/property-in-mohali.htm" title="Property in Mohali">Property in Mohali</a></p>
+                           </li>
+                           <li>
+                              <b class="b2 fl"></b>
+                              <p class="ofh"><a href="http://www.chandigarhhouse.com/property-in-zirakpur.htm" title="Property in Zirakpur">Property in Zirakpur</a></p>
+                           </li>
+                        </ul>
+                        <p class="cb"></p>
+                     </div>
+                  </div>
+               </div>
+               <div class="column_Box mc qc">
+                  <div class="h h2">
+                     <b class="plusMinus"></b> 
+                     <p class="b dif rHead">Contact Us</p>
+                  </div>
+                  <div class="ic showHide_rp">
+                     <div class="uu lh13em">
+                        <p class="dif b">R S Real Estate</p>
+                        <p class="hr bdrB"></p>
+                        <p>SCF-19 Phase-7, 2nd Floor, Phase 7, Mohali, Punjab - 160062, India</p>
+                        <p class="hr bdrB"></p>
+                        <p><b>Mobile :</b> +91-9872023591</p>
+                        <p class="hr bdrB"></p>
+                        <p><b>E-mail :</b> <a href="mailto:chadha_in@yahoo.com">chadha_in@yahoo.com</a></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-2 wideColumn ip ls">
+               <br style="content:'';display:inline-block;width:100%;height:15px;">
+               <!-- header ends -->
+               <div class="thinFormat">
+                  <div class="fo mt30px mb30px cf_3img">
+                     <ul class="lsn fo idv_eqheight">
+                        <li>
+                           <div class="bdr cp box-sector-property" onclick="location.href='http://www.chandigarhhouse.com/rent/4-bhk-flats-apartments-sector-78-mohali_1222859.htm'" style="height: 298.4px;">
+                              <div class="clsifd_img lh0 pr img-zoom ofh"><img loading="lazy" src="https://rei.wlimg.com/prop_images/88260/1222859_1-350x350.jpg" width="262" height="349" alt="freehold"></div>
+                              <div class="p10px">
+                                 <div class="proj-dt">
+                                    <div class="proj-dtc">
+                                       <div class="proj-cont vat pl2px">
+                                          <p class="xlarge dif"><a href="http://www.chandigarhhouse.com/rent/4-bhk-flats-apartments-sector-78-mohali_1222859.htm" title="freehold">freehold</a></p>
+                                          <p class="mt7px alpha75"><span class="w20px dib alpha50"><i class="fa large fa-map-marker"></i></span> Sector 78 Mohali/Mohali</p>
+                                          <p class="alpha75 mt2px"><span class="w20px dib alpha50"><i class="fa fa-area-chart"></i></span> 2200 Sq.ft.</p>
+                                          <p class="mt5px"><span class="w20px dib alpha50"><i class="fa fa-building-o"></i></span> 4 BHK</p>
+                                       </div>
+                                    </div>
+                                    <div class="proj-dtc vat ar w120px">
+                                       <p class="fw6 xlarge"><i class="fa fa-inr"></i> 40,000</p>
+                                       <p class="mt10px"><a href="http://www.chandigarhhouse.com/rent/4-bhk-flats-apartments-sector-78-mohali_1222859.htm" class="dib p5px c3px ts0 pl15px pr15px buttonBig">More Details</a></p>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </li>
+                        <li>
+                           <div class="bdr cp box-sector-property" onclick="location.href='http://www.chandigarhhouse.com/rent/3-bhk-flats-apartments-acme-heights-colony-mohali_1222848.htm'" style="height: 298.4px;">
+                              <div class="clsifd_img lh0 pr img-zoom ofh"><img loading="lazy" src="https://rei.wlimg.com/prop_images/88260/1222848_1-350x350.jpeg" width="262" height="349" alt="freehold"></div>
+                              <div class="p10px">
+                                 <div class="proj-dt">
+                                    <div class="proj-dtc">
+                                       <div class="proj-cont vat pl2px">
+                                          <p class="xlarge dif"><a href="http://www.chandigarhhouse.com/rent/3-bhk-flats-apartments-acme-heights-colony-mohali_1222848.htm" title="freehold">freehold</a></p>
+                                          <p class="mt7px alpha75"><span class="w20px dib alpha50"><i class="fa large fa-map-marker"></i></span> Acme Heights Colony/Mohali</p>
+                                          <p class="alpha75 mt2px"><span class="w20px dib alpha50"><i class="fa fa-area-chart"></i></span> 1900 Sq.ft.</p>
+                                          <p class="mt5px"><span class="w20px dib alpha50"><i class="fa fa-building-o"></i></span> 3 BHK</p>
+                                       </div>
+                                    </div>
+                                    <div class="proj-dtc vat ar w120px">
+                                       <p class="fw6 xlarge"><i class="fa fa-inr"></i> 60,000</p>
+                                       <p class="mt10px"><a href="http://www.chandigarhhouse.com/rent/3-bhk-flats-apartments-acme-heights-colony-mohali_1222848.htm" class="dib p5px c3px ts0 pl15px pr15px buttonBig">More Details</a></p>
+                                    </div>
+                                 </div>
+                              </div>
+                           </div>
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+               <!-- footer -->
+            </div>
+         </div>
+         <br style="content:'';display:inline-block;width:100%;height:15px;">
+      </div>
+   </div>
+
+@include('FrontendPages.partials.footer')
+
+@endsection
